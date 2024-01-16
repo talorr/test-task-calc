70dfea6be12a923501c08882fbdb2a74f8d53827
talorr <talorr31@gmail.com>
2024-01-16 19:22:34 +0700

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
                 <img class="left-0 object-cover hidden lg:block fixed w-screen h-screen -z-10" src="https://i.ibb.co/DgZb0b9/image-13.png" alt="">
             </div>
             <div class="w-11/12 mt-20 h-screen">
-                <p class="mb-[35px] text-black text-[26px] md:text-4xl xl:text-5xl font-medium">Рассчитайте стоимость Вашего потолка онлайн!</p>
+                <p class="mb-[35px] text-black text-[26px] md:text-4xl xl:text-5xl font-medium">Рассчитайте стоимость Вашего потолка онлайн</p>
                 <Calculator></Calculator>
             </div>
         </div>   
